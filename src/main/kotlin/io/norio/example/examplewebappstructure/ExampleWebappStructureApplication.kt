95852b913a85173125c18e2e2daa254944328708
Norio <59291150+norio-io@users.noreply.github.com>
2021-11-25 19:30:05 +0900

    Add the initial code generated by Spring Initializer.

---
+++ src/main/kotlin/io/norio/example/examplewebappstructure/ExampleWebappStructureApplication.kt
@@ -0,0 +1,11 @@
+package io.norio.example.examplewebappstructure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampleWebappStructureApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExampleWebappStructureApplication>(*args)
+}
---
+++ src/test/kotlin/io/norio/example/examplewebappstructure/ExampleWebappStructureApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.norio.example.examplewebappstructure
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExampleWebappStructureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
